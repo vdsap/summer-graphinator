9511d8e940a1173d7160700642091bfbabb7e7b7
vdsap <thevdsap@gmail.com>
2025-07-01 15:53:55 +0300

    rewrite

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from gui import security_window, main_window
+import sys
+import cProfile
+import pstats
+from gui import security_window, MainWindow
+from PyQt6.QtWidgets import QApplication
 
-security = security_window()
-security.gui_security()
-main = main_window()
-main.gui_main()
+
+def main():
+    security = security_window()
+    security.gui_security()
+    app = QApplication([])
+    app_window = MainWindow()
+    app_window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    cProfile.run("main()", 'profile_output.txt')
+    p = pstats.Stats('profile_output.txt')
+    p.sort_stats('cumulative').print_stats(10)
+
